50ef231c4da3eb41c5e1958d5997471d916caf6c
patrick <patrick.cadet@maison.fr>
2024-04-25 17:06:09 +0200

    initialisation

---
+++ web.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import fonctions
+
+todos = fonctions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    fonctions.write_todos(todos)
+
+
+st.title("My Todo App")
+st.subheader("C'est ma Todo Liste")
+st.write("Une belle application")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        fonctions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="", placeholder="Add a Todo here ...",
+              on_change=add_todo, key='new_todo')
+
